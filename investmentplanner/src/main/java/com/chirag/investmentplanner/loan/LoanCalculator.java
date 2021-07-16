a153f37705137d16662748cd6e71f7a9b7eb535b
chiragj <chirag.j@ezdi.us>
2021-07-16 11:52:02 +0530

    put limit of tax exemption

--- investmentplanner/src/main/java/com/chirag/investmentplanner/loan/LoanCalculator.java
+++ investmentplanner/src/main/java/com/chirag/investmentplanner/loan/LoanCalculator.java
@@ -9,6 +9,11 @@ import com.chirag.investmentplanner.investment.InvestmentCalculator;
 
 public class LoanCalculator {
 
+	// Get exempt in incometax of 1.5 lac in principal & 2 lac in Interest
+	
+	private static final int MAX_EXEMPTABLE_PRINCIPAL_AMOUNT = 150000;
+	private static final int MAX_EXEMPTABLE_LOAN_INTEREST_AMOUNT = 200000;
+	private static final int MAX_EXEMPTABLE_INVESTEMENT_INTEREST_AMOUNT = 10000;
 	/**
 	 * 
 	 * @param emiAmount
@@ -16,11 +21,11 @@ public class LoanCalculator {
 	 * @param loanAmount
 	 * @param incomeTaxSlab
 	 * @param annualInterestRateForInvetment
-	 * @param partPaymentInfo 
+	 * @param partPaymentInfo
 	 * @return
 	 */
-	public static FinalLoanPaymentInfoBean getLoanPaymentInfo(double emiAmount, float annualInterestRate, double loanAmount, float incomeTaxSlab, float annualInterestRateForInvetment)
-	{
+	public static FinalLoanPaymentInfoBean getLoanPaymentInfo(double emiAmount, float annualInterestRate,
+			double loanAmount, float incomeTaxSlab, float annualInterestRateForInvetment) {
 		double remainingLoanAmount = loanAmount;
 		double totalPayedAmount = 0;
 		double totalPayedInterestAmount = 0;
@@ -30,45 +35,60 @@ public class LoanCalculator {
 		List<Double> principalPayedPerEmi = new ArrayList<Double>();
 		List<YearlyLoanPaymentInfoBean> yearlyLoanPaymentInfoBeans = new ArrayList<YearlyLoanPaymentInfoBean>();
 		YearlyLoanPaymentInfoBean yearlyLoanPaymentInfoBean = new YearlyLoanPaymentInfoBean();
-		while(remainingLoanAmount>0)
-		{
-			//double paymentAmount = emiAmount;
+		while (remainingLoanAmount > 0) {
+			// double paymentAmount = emiAmount;
 			totalPayedAmount = totalPayedAmount + emiAmount;
 			numOfEmi++;
-			
+
 			yearlyLoanPaymentInfoBean.addTotalPayedAmount(emiAmount);
-			double payedInterestAmount = InterestUtility.calculateInterestAmount(InterestUtility.calculateInterestRate(annualInterestRate, 1), remainingLoanAmount);
+			double payedInterestAmount = InterestUtility.calculateInterestAmount(
+					InterestUtility.calculateInterestRate(annualInterestRate, 1), remainingLoanAmount);
 			double payedPrincipalAmount = emiAmount - payedInterestAmount;
-			//double earnedInterestOnInvestment = InterestUtility.calculateInterestAmount(InterestUtility.calculateInterestRate(annualInterestRateForInvetment, 1), remainingLoanAmount);
-			//totalEarnedInterestOnInvestment = totalEarnedInterestOnInvestment+earnedInterestOnInvestment;
+			// double earnedInterestOnInvestment =
+			// InterestUtility.calculateInterestAmount(InterestUtility.calculateInterestRate(annualInterestRateForInvetment,
+			// 1), remainingLoanAmount);
+			// totalEarnedInterestOnInvestment =
+			// totalEarnedInterestOnInvestment+earnedInterestOnInvestment;
 			remainingLoanAmount = remainingLoanAmount - (payedPrincipalAmount);
 			totalPayedInterestAmount = totalPayedInterestAmount + payedInterestAmount;
 			interestPayedPerEmi.add(payedInterestAmount);
 			principalPayedPerEmi.add(payedPrincipalAmount);
 			yearlyLoanPaymentInfoBean.addTotalInterestPayedAmount(payedInterestAmount);
 			yearlyLoanPaymentInfoBean.addTotalPayedPrincipalAmount(payedPrincipalAmount);
-			System.out.println("remianing loan amount : "+remainingLoanAmount+", payedInterestAmount : "+payedInterestAmount);
-			if(remainingLoanAmount > loanAmount)
-			{
-				System.out.println("Wrong remaining amount : "+remainingLoanAmount);
+			System.out.println("remianing loan amount : " + remainingLoanAmount + ", payedInterestAmount : "
+					+ payedInterestAmount);
+			if (remainingLoanAmount > loanAmount) {
+				System.out.println("Wrong remaining amount : " + remainingLoanAmount);
 				break;
 			}
-			
-			if(numOfEmi%12 == 0)
-			{
+
+			if (numOfEmi % 12 == 0) {
 				yearlyLoanPaymentInfoBean.setRemainigLoanAmount(remainingLoanAmount);
 				yearlyLoanPaymentInfoBeans.add(yearlyLoanPaymentInfoBean);
 				yearlyLoanPaymentInfoBean = new YearlyLoanPaymentInfoBean();
 			}
 		}
-		//Calculate investment interest as invest loan amount as FD from first day of loan.
-		totalEarnedInterestOnInvestment = InvestmentCalculator.cumulativeInterestInvestment(loanAmount, annualInterestRateForInvetment, numOfEmi, 3).getEarnedInterestAmount();
-		double totalSavedIncomeTax = (totalPayedAmount-totalEarnedInterestOnInvestment) * incomeTaxSlab/100;
-		return new FinalLoanPaymentInfoBean(totalPayedAmount, totalPayedInterestAmount, interestPayedPerEmi, principalPayedPerEmi, yearlyLoanPaymentInfoBeans, numOfEmi, totalEarnedInterestOnInvestment, totalSavedIncomeTax);
+		// Calculate investment interest as invest loan amount as FD from first day of
+		// loan.
+		totalEarnedInterestOnInvestment = InvestmentCalculator
+				.cumulativeInterestInvestment(loanAmount, annualInterestRateForInvetment, numOfEmi, 3)
+				.getEarnedInterestAmount();
+		double totalSavedIncomeTax = getSavedIncomeTax(yearlyLoanPaymentInfoBeans, incomeTaxSlab);
+		return new FinalLoanPaymentInfoBean(totalPayedAmount, totalPayedInterestAmount, interestPayedPerEmi,
+				principalPayedPerEmi, yearlyLoanPaymentInfoBeans, numOfEmi, totalEarnedInterestOnInvestment,
+				totalSavedIncomeTax);
 	}
-	
-	/*
-	 * public double getSavedIncomeTax() { return
-	 * (totalPayedAmount-totalEarnedInterestOnInvestment) * incomeTaxSlab/100; }
-	 */
+
+	private final static double getSavedIncomeTax(List<YearlyLoanPaymentInfoBean> yearlyLoanPaymentInfoBeans, float incomeTaxSlab) {
+		double totalSavedIncomeTax = 0;
+		for (YearlyLoanPaymentInfoBean yearlyLoanPaymentInfoBean : yearlyLoanPaymentInfoBeans) {
+			double principalAmount = yearlyLoanPaymentInfoBean.getTotalPayedPrincipalAmount();
+			double interestAmount = yearlyLoanPaymentInfoBean.getTotalInterestPayedAmount();
+			principalAmount = principalAmount > MAX_EXEMPTABLE_PRINCIPAL_AMOUNT ? MAX_EXEMPTABLE_PRINCIPAL_AMOUNT : principalAmount;
+			interestAmount = interestAmount > MAX_EXEMPTABLE_LOAN_INTEREST_AMOUNT ? MAX_EXEMPTABLE_LOAN_INTEREST_AMOUNT : interestAmount;
+			totalSavedIncomeTax = totalSavedIncomeTax + ((principalAmount+interestAmount)*incomeTaxSlab/100);
+		}
+		return totalSavedIncomeTax;
+	}
+
 }
