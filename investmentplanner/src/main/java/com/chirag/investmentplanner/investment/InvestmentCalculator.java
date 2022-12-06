8271a4f11c89e03333a823ac9bbc70d928edd259
chirag <chirag@raapid.ai>
2022-12-06 15:21:16 +0530

    add new algo to recuring deposit

--- investmentplanner/src/main/java/com/chirag/investmentplanner/investment/InvestmentCalculator.java
+++ investmentplanner/src/main/java/com/chirag/investmentplanner/investment/InvestmentCalculator.java
@@ -75,5 +75,48 @@ public class InvestmentCalculator {
 		return new InvestmentReturnInfoBean(totalInvestedAmount, totalEarnedInterestAmount, totalAmount);
 	}
 	
+	/**
+	 * This method will calculate investmentReturn for Recurring investment (i.e SIP, LIC policy)
+	 * @param initialAmount Amount which you pay at a time of join policy.
+	 * @param recurringAmount Amount which you pay periodically (i.e in LIC you pay 23k)
+	 * @param recurringFrequencyInMonths How frequently you invest (i.e in LIC you pay 23k yearly)
+	 * @param paymentPeriodInMonths  for how many months you pay periodic amount. (i.e you pay for 5 years but your policy got matured at 7th year)
+	 * @param maturityPeriodInMonths Policy duration
+	 * @param annualInterestRate Annual interest rate
+	 * @param interestPeriodInMonths Time duration, after which bank give you an interest. (i.e quarterly, 6 months, yearly)
+	 * 
+	 * For verification : https://www.creditfinanceplus.com/calculators/calculate-future-value-investments-compound-interest.php#help-calculators
+	 * @return
+	 */
+	public static InvestmentReturnInfoBean cumulativeInterestRecurringInvestmentAlgo2(double initialAmount, double recurringAmount, int recurringFrequencyInMonths, int paymentPeriodInMonths, int maturityPeriodInMonths, float annualInterestRate, int interestPeriodInMonths)
+	{
+		double totalInvestedAmount = initialAmount;
+		double totalAmount = initialAmount;
+		double totalEarnedInterestAmount = 0;
+		if(recurringFrequencyInMonths%interestPeriodInMonths != 0) {
+			final String errorMsg = "recurringFrequencyInMonths is not in mpltiply of interestPeriodInMonths";
+			System.out.println(errorMsg);
+			throw new RuntimeException(errorMsg);
+		}
+		int numOfRecuringPayment = (paymentPeriodInMonths/recurringFrequencyInMonths)-1;
+		for(int recurringIndex =0;  recurringIndex <= numOfRecuringPayment; recurringIndex++) {
+			if(recurringIndex > 0) {
+				totalAmount += recurringAmount; 
+				totalInvestedAmount += recurringAmount;
+			}
+			final InvestmentReturnInfoBean recurringAmountInvestmentInfoBean = cumulativeInterestInvestment(totalAmount, annualInterestRate, recurringFrequencyInMonths, interestPeriodInMonths);
+			System.out.println(recurringIndex + " Recurring investment info : "+recurringAmountInvestmentInfoBean+ " totalInverstedAmmount : "+totalInvestedAmount);
+			totalAmount = recurringAmountInvestmentInfoBean.getMaturityAmount();
+			totalEarnedInterestAmount = totalEarnedInterestAmount + recurringAmountInvestmentInfoBean.getEarnedInterestAmount();
+		}
+		if(maturityPeriodInMonths>paymentPeriodInMonths) {
+			final InvestmentReturnInfoBean recurringAmountInvestmentInfoBean = cumulativeInterestInvestment(totalAmount, annualInterestRate, maturityPeriodInMonths-paymentPeriodInMonths, interestPeriodInMonths);
+			System.out.println("Interest enrnerd after payment preiod investment info : "+recurringAmountInvestmentInfoBean);
+			totalAmount = recurringAmountInvestmentInfoBean.getMaturityAmount();
+			totalEarnedInterestAmount = totalEarnedInterestAmount + recurringAmountInvestmentInfoBean.getEarnedInterestAmount();
+		}
+		return new InvestmentReturnInfoBean(totalInvestedAmount, totalEarnedInterestAmount, totalAmount);
+	}
+	
 	
 }
